db769c1aabcbeb6ff0816002cca762956ebaf45f
zhaoyilun <zhaoyilun@outlook.com>
2020-12-14 15:11:47 +0800

    update

--- utils/new.go
+++ utils/new.go
@@ -5,42 +5,46 @@ import (
 	"os"
 )
 
-const suffix =".ez"
+//定义文件后缀
+const suffix = ".ez"
 
-func CreateFile(title string)error{
+//CreateFile 创建一个新的文件
+func CreateFile(title string) error {
 	//检查文件是否存在
-	fileName:=ezTitle(title)
-	_,err:=os.Stat(fileName)
-	if err==nil{
+	fileName := ezTitle(title)
+	_, err := os.Stat(fileName)
+	if err == nil {
 		fmt.Println("Failed to create. ")
-		fmt.Println("Warning: "+fileName+" is already created,please check it!")
+		fmt.Println("Warning: " + fileName + " is already created,please check it!")
 		return err
 	}
 	//创建文件
-	fmt.Println("Creating a new file named: ",fileName)
-	newFile,err:=os.Create(fileName)
-	if err!=nil{
+	fmt.Println("Creating a new file named: ", fileName)
+	newFile, err := os.Create(fileName)
+	if err != nil {
 		fmt.Println("Failed to create. ")
 		return err
 	}
-	err=newFile.Close()
-	if err !=nil{
-		fmt.Println("Failed to create file: "+fileName)
+	err = newFile.Close()
+	if err != nil {
+		fmt.Println("Failed to create file: " + fileName)
 		return err
 	}
-	fmt.Println(fileName+" creat complete!")
+	fmt.Println(fileName + " creat complete!")
 
 	//再次检查是否创建成功
-	_,err=os.Stat(fileName)
+	_, err = os.Stat(fileName)
 	if err != nil {
 		fmt.Println("Wow! the new file created just now does not exit!")
 		return err
 	}
 	return nil
 }
-func ezTitle(title string) string{
-	if len(title) >= len(suffix) && title[len(title)-len(suffix):] != suffix{
-		return title+suffix
+
+//ezTitle 检查文件名是否符合所需格式
+func ezTitle(title string) string {
+	if len(title) >= len(suffix) && title[len(title)-len(suffix):] != suffix {
+		return title + suffix
 	}
 	return title
 }
